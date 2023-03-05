d5bdbe3cdfd3e2141a64b7ae6fa91f6914f54158
CoderAbhi27 <abhinavkr327@gmail.com>
2023-03-05 17:05:41 +0530

    second commit

--- app/src/main/java/com/example/letmeknow/MainActivity.kt
+++ app/src/main/java/com/example/letmeknow/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.letmeknow
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import com.example.letmeknow.databinding.ActivitySignInBinding
 import com.google.firebase.auth.FirebaseAuth
 
@@ -17,9 +19,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val logout: Button = findViewById(R.id.logOut)
         logout.setOnClickListener{
-            firebaseAuth.signOut()
-            val intent = Intent(this, SignInActivity::class.java)
-            startActivity(intent)
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("LOG OUT")
+            builder.setMessage("Are you sure?")
+            builder.setPositiveButton("LOG OUT", DialogInterface.OnClickListener{dialog, which->
+                firebaseAuth.signOut()
+                val intent = Intent(this, SignInActivity::class.java)
+                startActivity(intent)
+            })
+            builder.setNegativeButton("Cancel", DialogInterface.OnClickListener{dialog, which-> })
+            val alertDialog: AlertDialog = builder.create()
+            alertDialog.show()
         }
 
     }
